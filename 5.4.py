fb5c6ba0752524af68cc75cbea6fbe3d2cfc5107
SoloVoice <a.v.rustamov@gmail.com>
2020-10-22 16:26:53 +0300

    +5.4

--- 5.4.py
+++ 5.4.py
@@ -1,7 +1,12 @@
 lang_dict = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
 with open("text_4.txt", 'r', encoding="utf-8") as file:
-    content = file.readlines()
-ass = []
-for el in content:
-    ass.append(el.replace("\n", ""))
-print(ass)
+    content = []
+    for el in file.readlines():
+        content.append(el.split(" - "))
+print(content)
+output_content = []
+for i in content:
+    i[0] = lang_dict.get(i[0])
+    output_content.append(' - '.join(i))
+with open("text_new_4.txt", 'w', encoding="utf-8") as output_file:
+    output_file.writelines(output_content)
